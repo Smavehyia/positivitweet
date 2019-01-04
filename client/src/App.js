12a999c0bbe2dd3384476b6a47f70db264ac0225
Smavehyia <shahidsmavia@gmail.com>
2019-01-03 19:54:38 -0500

    adding a router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+  } from 'react-router-dom'
 import './App.css';
+import Home from './Home/home'
 
 class App extends Component {
   state = {
@@ -33,35 +38,11 @@ class App extends Component {
   };
 render() {
     return (
+    <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
+      <Route path="/" exact component={Home}/>
       </div>
+      </Router>
     );
   }
 }
---
+++ client/src/Home/home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Positivitweet</h1>
+                <p>Find out the level of positivity demonstrated through your tweets by a click of a button. Begin the button whenever you're ready</p>
+                <button>Let's go!</button>
+            </div>
+        )
+    }
+}
+
+export default Home;
