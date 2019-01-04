9a0fe1a939d73b2648c8e6a73db08a3635135e34
Smavehyia <shahidsmavia@gmail.com>
2019-01-03 22:23:13 -0500

    client side auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
 import {
     BrowserRouter as Router,
     Route,
@@ -7,17 +8,62 @@ import {
 import './App.css';
 import Home from './Home/home'
 
+const API_URL = 'http://localhost:3000';
+const socket = io(API_URL);
+
 class App extends Component {
-  state = {
-    response: '',
-    post: '',
-    responseToPost: '',
-  };
+    constructor() {
+        super()
+        this.state = {
+            user: {},
+            disabled: '',
+        }
+        this.popup = null;
+    }
   componentDidMount() {
+    socket.on('user', user => {
+        this.popup.close()
+        this.setState({user})
+    })
     this.callApi()
       .then(res => this.setState({ response: res.express }))
       .catch(err => console.log(err));
   }
+
+  checkPopup = () => {
+      const check = setInterval(() => {
+          const { popup } = this;
+          if (!popup || popup.closed || popup.closed === undefined ) {
+              clearInterval(check)
+              this.setState({ disabled: ''})
+          }
+      }, 1000)
+  }
+  openPopup = () => {
+    const width = 600, height = 600
+    const left = (window.innerWidth / 2) - (width / 2)
+    const top = (window.innerHeight / 2) - (height / 2)
+    
+    const url = `${API_URL}/twitter?socketId=${socket.id}`
+
+    return window.open(url, '',       
+      `toolbar=no, location=no, directories=no, status=no, menubar=no, 
+      scrollbars=no, resizable=no, copyhistory=no, width=${width}, 
+      height=${height}, top=${top}, left=${left}`
+    )
+  }
+  startAuth = () => {
+    if (!this.state.disabled) {  
+      this.popup = this.openPopup()  
+      this.checkPopup()
+      this.setState({disabled: 'disabled'})
+    }
+  }
+  closeCard = () => {
+    this.setState({user: {}})
+  }
+
+
   callApi = async () => {
     const response = await fetch('/api/hello');
     const body = await response.json();
@@ -37,12 +83,24 @@ class App extends Component {
     this.setState({ responseToPost: body });
   };
 render() {
+    const { name, photo} = this.state.user
+    const { disabled } = this.state
+
+    const Home = () => (
+        <div>
+            <h1>Positivitweet</h1>
+            <p>Find out the level of positivity demonstrated through your tweets by a click of a button. Begin the button whenever you're ready</p>
+            <button onClick={this.startAuth}>Let's go!</button>
+        </div>
+    )
     return (
-    <Router>
+     <Router>
       <div className="App">
+      <h1>{name}</h1>
+      {this.state.disabled? <button onClick={this.closeCard}>Close</button> : null}
       <Route path="/" exact component={Home}/>
       </div>
-      </Router>
+      </Router> 
     );
   }
 }
